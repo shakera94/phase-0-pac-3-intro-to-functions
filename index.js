007eef9342cd85a24c92b0f6c932fd1cbada4f88
shakera <sanderso_4516574@yahoo.com>
2021-03-12 11:42:34 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log('Hello!');
+}
+sayHello();
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+function sayGoodbyeToJulio() {
+    console.log("Goodbye, Julio!");
+  }
+  sayGoodbyeToJulio();
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }  
+  say("Julio", "hello");
+function add(x, y) {    
+  return x + y;  
+}
+console.log(add(1,2));
+
+
